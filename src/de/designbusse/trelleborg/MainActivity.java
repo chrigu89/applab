732c6b84193228ab5d5c2f29ddfdf843f580bb31
Christian Busse <cb@design-busse.de>
2014-05-28 08:55:55 +0200

    Update External

--- src/de/designbusse/trelleborg/MainActivity.java
+++ src/de/designbusse/trelleborg/MainActivity.java
@@ -9,12 +9,17 @@ import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends DroidGap {
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		super.setIntegerProperty("splashscreen", R.drawable.splash);
-		super.loadUrl("http://applab.thenetworks.de/", 3000);
-	}
-
+    @Override
+public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("http://applab.thenetworks.de/index.html");
+        //setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
 }

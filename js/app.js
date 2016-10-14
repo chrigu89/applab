6c1f62daba2f28ea3739eadfba291c360827b8d3
chrigu89 <cb@design-busse.de>
2016-10-14 17:42:41 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -47,7 +47,16 @@ var init = {
 			final_token = data.registrationId;
 			window.localStorage.setItem("token", final_token);
 			
-			$('#website').attr("src", "http://applab.thenetworks.de/?login=appuser&token="+final_token+"&os=2&push=1");
+			var os = 1;
+			if(final_token.length == 64) {
+				os = 1;
+				//Apple
+			} else {
+				os = 2;
+				//Android
+			}
+			
+			$('#website').attr("src", "http://applab.thenetworks.de/?login=appuser&token="+final_token+"&os='+os+'&push=1");
 			
 		});
 		

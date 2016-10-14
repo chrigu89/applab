a63a64eadc7cae8963cbad7e189a86525159f57f
chrigu89 <cb@design-busse.de>
2016-10-14 13:35:14 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -46,19 +46,17 @@ var init = {
 		push.on('registration', function(data) {
 			final_token = data.registrationId;
 			window.localStorage.setItem("token", final_token);
-			alert('token'+final_token);
-			
+			alert(final_token);
 			$.ajax({
 				type: 'GET',
-				url: 'http://ncore.webentwicklungsserver.com/admin?login=appuser',
-				data:  { os: 2, push: 1, token: final_token },
+				url: 'http://ncore.webentwicklungsserver.com/de/admin?login=appuser',
+				data:  { puash: 1, os: 2, token: final_token },
 				crossDomain: true,
 				cache: false,
 				success: function(response) {
-					alert('response'+response);
+					
 				}
 			});
-			
 		});
 		
 		push.on('notification', function(data) {				
@@ -88,13 +86,51 @@ function onFileSystemSuccess(fileSystem) {
 		onGetFileSuccess,
 		onFail
 	);
+	/*alert(fileSystem.root.toURL());
+	alert(fileSystem.root.toInternalURL());
+	alert(fileSystem.root.nativeURL);*/
 	
 }
 
 //  Hole Root Verzeichnis
 function onGetFileSuccess(fileEntry) {
 	var path = fileEntry.toURL().replace('index.html', ''); // URL der offenen Datei!
+	/*
+		Download starten
+	
+	var fileTransfer = new FileTransfer();
+	fileEntry.remove();
+	fileTransfer.download(
+		'http://apps.design-busse.de/ngk/images/pdf/leitfaden.pdf',
+		path + 'leitfaden.pdf',
+		function(file) {
+			//alert('Download erfolgreich, datei wird geöffnet: ' + file.toURI());
+			showPDF(file.toURI());
+		},
+		function(error) {
+			alert('download error source ' + error.source);
+			alert('download error target ' + error.target);
+			alert('upload error code: ' + error.code);
+		}
+	);*/
 }
+
+// PDF Anzeigen
+function showPDF(url) {
+	//window.resolveLocalFileSystemURI(url, onResolveSuccess, onFail); <- Klappt
+	cordova.plugins.fileOpener2.open(
+		url, // e.g. '/var/mobile/Applications/XXXXXXXXX/Library/files/mypdf.pdf'
+		'application/pdf', {
+			error: function(errorObj) {
+				alert('Error status: ' + errorObj.status + ' - Error message: ' + errorObj.message);
+			},
+			success: function() {
+				//alert('Datei erfolgreich geladen');
+			}
+		}
+	);
+}
+
 
 function onResolveSuccess(fileEntry){
 	alert(fileEntry.name);	
@@ -128,3 +164,115 @@ function successHandler (result) {
 function errorHandler (error) {
 	//$("#app-status-ul").append('<li>error:'+ error +'</li>');
 }
+
+var onSettings = function() {
+	var token = window.localStorage.getItem("token");
+	$.ajax({
+		type: 'GET',
+		dataType: "json",
+		url: 'http://apps.design-busse.de/ngk/ios/api.php?rquest=get_user',
+		data:  { token: token },
+		crossDomain: true,
+		cache: false,
+		success: function(response) {
+			var status = response.status;
+			if(status == 1) {
+				$('.status').addClass('on');
+			} else {
+				$('.status').addClass('off');
+			}
+			$('.id').text(response.id);
+			
+		}
+	});
+
+	return;
+};
+
+var changeStatus = function(status) {
+	var token = window.localStorage.getItem("token");
+	
+	$.ajax({
+		type: 'GET',
+		url: 'http://apps.design-busse.de/ngk/ios/api.php?rquest=change_status',
+		data:  { token: token, status: status },
+		crossDomain: true,
+		cache: false,
+		success: function(response) {
+			alert('Der Status wurde geändert');			
+		}
+	});
+
+	return;
+};
+
+
+$('.status').click(function(){
+	
+	if($(this).hasClass('off')) {
+		changeStatus(1);
+		$(this).removeClass('off');
+		$(this).addClass('on');
+	} else {
+		changeStatus(0);
+		$(this).removeClass('on');
+		$(this).addClass('off');
+	}
+	
+});
+
+var onNews = function() {
+		
+
+	$.ajax({
+		type: 'GET',
+		dataType: "json",
+		url: 'http://neusser-grenadierkorps.de/webapp/api.php',
+		data:  { "action": "news" },
+		crossDomain: true,
+		cache: false,
+		success: function(response) {
+			
+			var news = response;
+			var ausgabe = '<ul>';
+			for (var i=0; i < news.length; i++) {
+				var current = news[i];
+				ausgabe += '<li><strong>'+current.date+'</strong><br>'+current.subheadline+'<br>'+current.text+'</li>';
+			}
+			ausgabe += '<ul>';
+			$( "#news" ).html( ausgabe );
+			
+			onReady();
+			
+		}
+	});
+
+	return;
+};
+
+var onTwitter = function() {
+	
+	$.ajax({
+		type: 'GET',
+		dataType: "json",
+		url: 'http://neusser-grenadierkorps.de/webapp/api.php',
+		data:  { "action": "twitter" },
+		crossDomain: true,
+		cache: false,
+		success: function(response) {
+			
+			var news = response;
+			var ausgabe = '<ul>';
+			for (var i=0; i < news.length; i++) {
+				var current = news[i];
+				ausgabe += '<li><strong>'+current.date+'</strong><br>'+current.text+'</li>';
+			}
+			ausgabe += '<ul>';
+			$( "#twitter" ).html( ausgabe );
+			
+		}
+	});
+
+	return;
+};
+

483187443d3abfa83feb98d5ad9d6222f541ec1d
cb@design-busse.de <DTxhruKb5q>
2017-12-28 17:34:28 +0100

    Update keepAwake

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,6 @@ jQuery(document).ready(function() {
 	if($('#website').attr("src") == "loading.html") {
 		$('#website').attr("src", "http://applab.thenetworks.de/?notoken");	
 	} 
+	
+	window.plugins.insomnia.keepAwake();
 });
